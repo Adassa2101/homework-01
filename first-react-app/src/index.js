2bd40c6ef2be0543b737f23f8ca13a4277ff9ef6
System Administrator <root@macs-MacBook-Pro.local>
2021-09-05 18:15:47 +0300

    Initial commit

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const element = <h1>Hello World! Меня зовут Adassa Meir.</h1>;
+const element = <h1>Hello World! Меня зовут Adassa.</h1>;
 ReactDom.render(element, document.getElementById("root"));
+
